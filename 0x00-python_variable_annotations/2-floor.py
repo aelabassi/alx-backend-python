3ac49ed7859cec999a82adbc35888dffbdd9e03d
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-10-10 09:54:45 +0100

    type-annotated function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Type-annotated function floor which takes a float n as
+ argument and returns the floor of the float."""
+
+
+def floor(n: float) -> int:
+    """Returns the floor of the float."""
+    return int(n)
