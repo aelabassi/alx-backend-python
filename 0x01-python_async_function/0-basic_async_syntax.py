e686036136f2a2200f9cd8a15a20996bf0c2fd7c
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-10-14 16:02:49 +0100

    simple courotine function #0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,7 +7,7 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-	""" async function  """
-	delay = random.uniform(0, max_delay)
-	await asyncio.sleep(delay)
-	return delay
+    """ async function """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
