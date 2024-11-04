d278071cb7c8e4d47c38e149dc519ac28994c842
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-11-04 11:56:46 +0100

    unittests for utils: memoiziatiom #3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -47,20 +47,21 @@ class TestMemoize(unittest.TestCase):
         """ Test memoize function """
         class TestClass:
             """ TestClass Class """
-            def __init__(self):
-                """ Init method of TestClass """
-                self.a_method_called = 0
-
-            @memoize
             def a_method(self):
                 """ a_method method """
-                self.a_method_called += 1
                 return 42
 
-        test_object = TestClass()
-        self.assertEqual(test_object.a_method, 42)
-        self.assertEqual(test_object.a_method, 42)
-        self.assertEqual(test_object.a_method_called, 1)
+            @memoize
+            def a_property(self):
+                """ a_property method """
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock:
+            test_class = TestClass()
+            mock.return_value = 42
+            self.assertEqual(test_class.a_property, 42)
+            self.assertEqual(test_class.a_property, 42)
+            mock.assert_called_once()
 
 
 
