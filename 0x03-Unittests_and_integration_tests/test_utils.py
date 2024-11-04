97e60bad4c8604dee03e77fe19e60bc1308a77e3
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-11-04 11:21:38 +0100

    unittests for utils #0

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Unittests for utils """
+import unittest
+from parameterized import parameterized, parameterized_class
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ TestAccessNestedMap Class """
+    @parameterized.expand([
+        ({'a': 1}, ('a',), 1),
+        ({'a': {'b': 2}}, ('a',), {'b': 2}),
+        ({'a': {'b': 2}}, ('a', 'b'), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """ Test access_nested_map function """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ('a',), KeyError),
+        ({'a': 1}, ('a', 'b'), KeyError),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """ Test access_nested_map function with exception """
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
